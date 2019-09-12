237e1debf89b42cc8fb8d7b70466830e86ae6be6
Maxime BAKO <chocoboynrg@gmail.com>
2019-09-12 02:19:24 +0200

    new 2

--- resources/views/consultations/create.blade.php
+++ resources/views/consultations/create.blade.php
@@ -2,22 +2,13 @@
 @section('content')
     <h1>Creer consultation</h1>
     {{$patient->id}}
-    {!! Form::open(['action' => 'ConsultationsController@store', 'method' => 'POST']) !!}
-        <div class="form-group">
-            {{Form::label('accompagnant','Nom Accompagnant')}}
-            {{Form::text('accompagnant','',['class'=>'form-control','placeholder'=>'Nom et prenom de l\'accompagnant'])}}
-        </div>
-        <div class="form-group">
-            {{Form::label('contactaccompagnant','Contact')}}
-            {{Form::text('contactaccompagnant','',['class'=>'form-control','placeholder'=>'Numero de telephone'])}}
-        </div>
-        <div class="form-group">
-            {{Form::label('reference','Reference')}}
-            {{Form::text('reference','',['class'=>'form-control','placeholder'=>'venu de la part de ?'])}}
-        </div>
-        <div class="form-group">
-            {{Form::hidden('patient_id', $patient->id)}}
-        </div>
-        {{Form::submit('Creer', ['class'=>'btn btn-primary'])}}
-    {!! Form::close() !!}
+    <form  action="consultations" method="POST">
+        <label>Accompagnant:</label><input type="text" name="accompagnant" placeholder="Nom et prenom" class="form-control">
+        <label>Contact:</label><input type="text" name="contactaccompagnant" placeholder="Contact de l\'accompagnant" class="form-control">
+        <label>Reference:</label><input type="text" name="reference" placeholder="venu de la part de?" class="form-control">
+        <input type="hidden" name="patient_id" value={{$patient->id}}>
+        <input type="submit" value="creer" class="btn btn-primary">
+        @csrf
+    </form>
+
 @endsection
